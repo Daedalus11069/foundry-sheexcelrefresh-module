0cdeea7fe48c705310d91f15442d0846f666ce8a
muhahahahe <muhahahahe@gmail.com>
2024-07-11 17:07:16 +0200

    finish v0.3.1

--- src/scripts/sheexcel.js
+++ src/scripts/sheexcel.js
@@ -31,7 +31,6 @@ Hooks.once("init", async function () {
             return null;
         }
     }
-
 });
 
 Hooks.once("ready", async function () {
@@ -46,7 +45,8 @@ class SheexcelActorSheet extends ActorSheet {
         this._cellReferences = this.actor.getFlag("sheexcel", "cellReferences") || [];
         this._sheetId = this.actor.getFlag("sheexcel", "sheetId") || null;
         this._currentSheetName = this.actor.getFlag("sheexcel", "currentSheetName") || null;
-        this._sheetNames = [];
+        this._sheetNames = this.actor.getFlag("sheexcel", "sheetNames") || [];
+        this._iframe = null;
         this._refetchAllCellValues();
     }
 
@@ -57,6 +57,13 @@ class SheexcelActorSheet extends ActorSheet {
             width: 1200,
             height: 700,
             resizable: true,
+            tabs: [
+                {
+                    navSelector: ".sheexcel-sheet-tabs",
+                    contentSelector: ".sheexcel-sidebar",
+                    initial: "settings",
+                },
+            ],
         });
     }
 
@@ -67,15 +74,27 @@ class SheexcelActorSheet extends ActorSheet {
         data.hideMenu = this.actor.getFlag("sheexcel", "hideMenu") ?? true;
         data.sidebarCollapsed = this._sidebarCollapsed;
         data.cellReferences = this._cellReferences;
+        data.sheetNames = this._sheetNames.length > 1 ? this._sheetNames : null;
+        data.currentSheetName = this._currentSheetName;
+        data.sheetId = this._sheetId;
+        data.adjustedReferences = this._cellReferences.map(cellRef => {
+            const sheetNames = this._sheetNames.reduce((acc, name) => {
+                acc[name] = name;
+                return acc;
+            }, {});
+            cellRef.sheetNames = sheetNames
+            if (cellRef.value.length > 10) {
+                cellRef.value = cellRef.value.slice(0, 10);
+            }
+            return cellRef;
+        });
 
+        console.log(data.adjustedReferences)
         return data;
     }
     
     async _refetchAllCellValues() {
-        if (!this._sheetId || !this._currentSheetName) {
-            console.error("Sheet ID or name not found. Make sure the Google Sheet URL is correct and a sheet is selected.");
-            return;
-        }
+        if (!this._sheetId || !this._currentSheetName) return;
 
         for (let i = 0; i < this._cellReferences.length; i++) {
             await this._updateCellValue(i);
@@ -93,140 +112,79 @@ class SheexcelActorSheet extends ActorSheet {
     activateListeners(html) {
         super.activateListeners(html);
 
-        html.find(".sheexcel-update-sheet").click(this._onUpdateSheet.bind(this));
-        html.find(".sheexcel-sidebar-toggle").click(this._onToggleSidebar.bind(this));
-        html.find(".sheexcel-add-reference").click(this._onAddCellReference.bind(this));
-        html.find(".sheexcel-remove-reference").click(this._onRemoveCellReference.bind(this));
-        html.find(".sheexcel-cell, .sheexcel-keyword").change(this._onCellReferenceChange.bind(this));
-        html.find(".sheexcel-sheet-select").change(this._onSheetSelect.bind(this));
+        html.find(".sheexcel-sheet-toggle").click(this._onToggleSidebar.bind(this));
+        html.find(".sheexcel-sheet-references").click(this._onToggleTab.bind(this));
+        html.find(".sheexcel-sheet-settings").click(this._onToggleTab.bind(this));
+        html.find(".sheexcel-setting-update-sheet").click(this._onUpdateSheet.bind(this));
+        html.find(".sheexcel-reference-add-button").click(this._onAddReference.bind(this));
+        html.on("click", ".sheexcel-reference-remove-save", this._onSaveReference.bind(this));
+        html.on("click", ".sheexcel-reference-remove-button", this._onRemoveReference.bind(this));
+        html.on("change", "#sheexcel-cell", this._onCellReferenceChange.bind(this));
+        html.on("change", "#sheexcel-keyword", this._onKeywordReferenceChange.bind(this));
+        html.on("change", "#sheexcel-sheet", this._onCellReferenceChange.bind(this));
 
-        const iframe = html.find(".sheexcel-iframe")[0];
-        this._setupZoom(html, iframe);
-        this._setupHideMenu(html, iframe);
+        this._iframe = html.find(".sheexcel-iframe")[0];
+        this._setupZoom(html, this._iframe);
+        this._setupHideMenu(html, this._iframe);
 
-        this._applyZoom(iframe, this._currentZoomLevel);
+        this._applyZoom(this._iframe, this._currentZoomLevel);
     }
 
-    async _onToggleSidebar(event) {
+    _onToggleSidebar(event) {
         event.preventDefault();
         this._sidebarCollapsed = !this._sidebarCollapsed;
 
-        const toggleButton = $(event.currentTarget);
-        toggleButton.text(
-            (this._sidebarCollapsed ? "◀ " : "▶ ") +
-                game.i18n.localize("SHEEXCEL.Sidebar"),
-        );
+        const icon = $(event.currentTarget.children[0]);
+
+        const collapsed = `<svg width="30" height="24" viewBox="0 -4 27 26" xmlns="http://www.w3.org/2000/svg">
+                    <rect x="1" y="1" width="25" height="20" fill="#eeeeee" stroke="#000000" stroke-width="2"/>
+                    <rect x="18" y="1" width="1" height="20" fill="#151515"/>
+                </svg>`;
+        const expanded = `<svg width="30" height="24" viewBox="0 -4 27 26" xmlns="http://www.w3.org/2000/svg">
+                    <rect x="1" y="1" width="25" height="20" fill="#eeeeee" stroke="#000000" stroke-width="2"/>
+                    <rect x="18" y="1" width="7" height="20" fill="#151515"/>
+                </svg>`;
+        icon.empty();
+        icon.append(this._sidebarCollapsed ? collapsed : expanded);
 
         const sidebar = this.element.find(".sheexcel-sidebar");
 
         sidebar.toggleClass("collapsed", this._sidebarCollapsed);
     }
 
-    async _onCellReferenceChange(event) {
-        const index = $(event.currentTarget).closest(".sheexcel-cell-reference").index();
-        const field = event.currentTarget.classList.contains("sheexcel-cell") ? "cell" : "keyword";
-
-        const oldKeyword = this._cellReferences[index].keyword;
-        this._cellReferences[index][field] = event.currentTarget.value;
-
-        if (field === "keyword" && oldKeyword !== this._cellReferences[index].keyword) {
-            await this._removeSheexcelData(oldKeyword);
-        }
-        
-        await this._updateCellValue(index);
-        
-    }
-
-    async _updateCellValue(index) {
-        if (!this._sheetId || !this._currentSheetName) {
-            console.error("Sheet ID or name not found. Make sure the Google Sheet URL is correct and a sheet is selected.");
-            return;
-        }
-
-        const cellRef = this._cellReferences[index];
-        if (!cellRef.cell) return;
-
-        try {
-            const value = await this._fetchCellValue(this._sheetId, this._currentSheetName, cellRef.cell);
-            cellRef.value = value;
-
-        } catch (error) {
-            console.error("Error fetching cell value:", error);
-        }
-    }
-
-    async _fetchCellValue(sheetId, sheetName, cellRef) {
-
-        const url = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?tqx=out:csv&sheet=${encodeURIComponent(
-            sheetName,
-        )}&range=${cellRef}`;
-
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const text = (await response.text()).trim().replace(/^"(.*)"$/, "$1");
-        return text;
-    }
-
-    async _onAddCellReference(event) {
+    _onToggleTab(event) {
         event.preventDefault();
-        this._cellReferences.push({ cell: "", keyword: "", value: "" });
-        this._renderSidebar();
-    }
+        if (!this._sidebarCollapsed) return;
+        this._sidebarCollapsed = false;
+        const icon = $(event.currentTarget.parentElement.children[0].children[0]);
 
-    async _onRemoveCellReference(event) {
-        event.preventDefault();
-        const index = $(event.currentTarget).closest(".sheexcel-cell-reference").index();
-        const removedRef = this._cellReferences.splice(index, 1)[0];
-        await this._removeSheexcelData(removedRef.keyword);
-        this._renderSidebar();
-    }
+        const collapsed = `<svg width="30" height="24" viewBox="0 -4 27 26" xmlns="http://www.w3.org/2000/svg">
+                    <rect x="1" y="1" width="25" height="20" fill="#eeeeee" stroke="#000000" stroke-width="2"/>
+                    <rect x="18" y="1" width="1" height="20" fill="#151515"/>
+                </svg>`;
+        const expanded = `<svg width="30" height="24" viewBox="0 -4 27 26" xmlns="http://www.w3.org/2000/svg">
+                    <rect x="1" y="1" width="25" height="20" fill="#eeeeee" stroke="#000000" stroke-width="2"/>
+                    <rect x="18" y="1" width="7" height="20" fill="#151515"/>
+                </svg>`;
+        icon.empty();
+        icon.append(this._sidebarCollapsed ? collapsed : expanded);
 
-    async _removeSheexcelData(keyword) {
-        if (keyword) {
-            const sheexcelData = duplicate(this.actor.system.sheexcel);
-            delete sheexcelData[keyword];
-        }
-    }
+        const sidebar = this.element.find(".sheexcel-sidebar");
 
-    async _renderSidebar() {
-        const sidebarContainer = this.element.find(".sheexcel-sidebar-content");
-        sidebarContainer.empty();
-
-        this._cellReferences.forEach((ref, index) => {
-            const cellRefHtml = `
-                <div class="sheexcel-cell-reference">
-                    <input type="text" class="sheexcel-cell" value="${ref.cell}" placeholder="${game.i18n.localize("SHEEXCEL.Cell")}">
-                    <input type="text" class="sheexcel-keyword" value="${ref.keyword}" placeholder="${game.i18n.localize("SHEEXCEL.Keyword")}">
-                    <span class="sheexcel-value">${ref.value || ""}</span>
-                    <button class="sheexcel-remove-reference"><i class="fas fa-trash"></i></button>
-                </div>
-            `;
-            sidebarContainer.append(cellRefHtml);
-        });
-
-        sidebarContainer.append(`<button class="sheexcel-add-reference">${game.i18n.localize("SHEEXCEL.AddReference")}</button>`);
-
-        this._activateSidebarListeners(sidebarContainer);
-
-        this.render(false)
-    }
-
-    _activateSidebarListeners(container) {
-        container.find(".sheexcel-add-reference").click(this._onAddCellReference.bind(this));
-        container.find(".sheexcel-remove-reference").click(this._onRemoveCellReference.bind(this));
-        container.find(".sheexcel-cell, .sheexcel-keyword").change(this._onCellReferenceChange.bind(this));
+        sidebar.toggleClass("collapsed", this._sidebarCollapsed);
     }
 
     async _onUpdateSheet(event) {
+        console.log("update sheet");
         event.preventDefault();
         let sheetUrl = this.element.find('input[name="sheetUrl"]').val();
-        console.log(sheetUrl);
 
-        if (sheetUrl && !sheetUrl.endsWith("/edit")) {
-            sheetUrl += "/edit";
+        if (!sheetUrl) {
+            this.sheetId = null;
+            this.currentSheetName = null;
+            this.sheetNames = [];
+            this.render(false);
+            return;
         }
 
         const sheetIdMatch = sheetUrl.match(/\/spreadsheets\/d\/([a-zA-Z0-9-_]+)/);
@@ -236,12 +194,15 @@ class SheexcelActorSheet extends ActorSheet {
         }
 
         await this.actor.setFlag("sheexcel", "sheetId", this._sheetId);
+        await this.actor.setFlag("sheexcel", "sheetName", this._currentSheetName);
+        await this.actor.setFlag("sheexcel", "sheetNames", this._sheetNames);
         await this.actor.setFlag("sheexcel", "sheetUrl", sheetUrl);
 
         this.render(false);
     }
 
     async _fetchSheetNames() {
+        console.log("fetching sheet names");
         if (!this._sheetId) return;
     
         const url = `https://docs.google.com/spreadsheets/d/${this._sheetId}/edit`;
@@ -265,37 +226,70 @@ class SheexcelActorSheet extends ActorSheet {
                     if (nameElement) {
                         const sheetName = nameElement.innerHTML.trim();
                         this._sheetNames.push(sheetName);
-                        
-                        if (tab.classList.contains('docs-sheet-active-tab')) {
-                            this._currentSheetName = sheetName;
-                        }
                     }
                 });
-    
-                if (!this._currentSheetName && this._sheetNames.length > 0) {
-                    this._currentSheetName = this._sheetNames[0];
-                }
+                this._currentSheetName = this._sheetNames[0];
             } else {
                 console.error("Sheet container not found");
             }
         } catch (error) {
             console.error("Error fetching sheet names:", error);
         }
+        return this._sheetNames;
     }
 
-    async _onSheetSelect(event) {
-        const newSheetName = event.target.value;
-        this._currentSheetName = newSheetName;
-        await this.actor.setFlag("sheexcel", "currentSheetName", newSheetName);
-        
-        for (let i = 0; i < this._cellReferences.length; i++) {
-            await this._updateCellValue(i);
+    _onAddReference(event) {
+        console.log("add reference");
+        event.preventDefault();
+        this._fetchSheetNames();
+        this._cellReferences.push({ sheet: this._currentSheetName, cell: "", keyword: "", value: "" });
+        const references = $(event.currentTarget.parentElement.previousElementSibling);
+        let sheets;
+        if (this._sheetNames.length > 1) {
+            sheets = `<select id="sheexcel-sheet" name="sheet">`;
+            sheets += this._sheetNames.map((name, i) => `<option value="${i}">${name}</option>`).join("");
+            sheets += "</select>";
+        } else {
+            sheets = `<span class="sheexcel-reference-cell-sheet">${this._currentSheetName}</span>`;
         }
+        const refHtml = `<div class="sheexcel-reference-cell">
+                    <input id="sheexcel-cell" type="text" value="" placeholder="${game.i18n.localize("SHEEXCEL.Cell")}">
+                    <input id="sheexcel-keyword" type="text" value="" placeholder="${game.i18n.localize("SHEEXCEL.Keyword")}">
+                    ${sheets}
+                    <div class="sheexcel-reference-remove">
+                        <button class="sheexcel-reference-remove-button">${game.i18n.localize("SHEEXCEL.Remove")}</button>
+                        <span class="sheexcel-reference-remove-value"></span>
+                    </div>
+                </div>`
+        references.append(refHtml);
+    }
+
+    _onRemoveReference(event) {
+        console.log("remove reference");
+        event.preventDefault();
+        const parent = event.currentTarget.parentElement.parentElement;
+
+        const siblings = Array.from(parent.parentElement.children);
+
+        const index = siblings.indexOf(parent);
+        this._cellReferences.splice(index, 1);
+        parent.remove();
+    }
+
+    _onSaveReference(event) {
+        console.log("save reference");
+        event.preventDefault();
+        this._refetchAllCellValues();
+        this._saveFlags();
     }
 
     async _fetchCellValue(sheetId, sheetName, cellRef) {
+        console.log("fetching cell value");
+        if (cellRef === "") return "";
 
-        const url = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?tqx=out:csv&sheet=${encodeURIComponent(sheetName)}&range=${cellRef}`;
+        const url = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?tqx=out:csv&sheet=${encodeURIComponent(
+            sheetName,
+        )}&range=${cellRef}`;
 
         const response = await fetch(url);
         if (!response.ok) {
@@ -306,9 +300,45 @@ class SheexcelActorSheet extends ActorSheet {
         return text;
     }
 
+    async _onCellReferenceChange(event) {
+        console.log("Cell changed");
+        const index = $(event.currentTarget).closest(".sheexcel-reference-cell").index();
+        if (event.currentTarget.id === "sheexcel-cell") {
+            this._cellReferences[index].cell = event.currentTarget.value;
+        } else {
+            this._cellReferences[index].sheet = event.target.value;
+            console.log(this._cellReferences[index].sheet);
+        }
+
+        if (this._cellReferences[index].cell && this._cellReferences[index].cell.length && this._cellReferences[index].sheet) {
+            this._cellReferences[index].value = await this._fetchCellValue(this._sheetId, this._cellReferences[index].sheet, this._cellReferences[index].cell);
+            const span = $(event.currentTarget.parentElement.children[3].lastElementChild);
+            span.text(this._cellReferences[index].value.slice(0, 10));
+        }
+        const ref = foundry.utils.duplicate(this.actor.system.sheexcel);
+        ref[this._cellReferences[index].keyword] = this._cellReferences[index].value;
+        this.actor.update({ "system.sheexcel": ref });
+    }
+
+    _onKeywordReferenceChange(event) {
+        console.log("Keyword changed");
+        const index = $(event.currentTarget).closest(".sheexcel-reference-cell").index();
+        this._cellReferences[index].keyword = event.currentTarget.value;
+        const ref = foundry.utils.duplicate(this.actor.system.sheexcel);
+        ref[this._cellReferences[index].keyword] = this._cellReferences[index].value;
+        this.actor.update({ "system.sheexcel": ref });
+    }
+
+    async _updateCellValue(i) {
+        console.log("Updating cell value", i);
+        const ref = this._cellReferences[i]
+        const value = await this._fetchCellValue(this._sheetId, ref.sheet || this._currentSheetName, ref.cell);
+        this._cellReferences[i].value = value;
+    }
+
     _setupZoom(html, iframe) {
-        const zoomSlider = html.find("#sheexcel-zoom-slider")[0];
-        const zoomValue = html.find("#sheexcel-zoom-value")[0];
+        const zoomSlider = html.find("#sheexcel-setting-zoom-slider")[0];
+        const zoomValue = html.find("#sheexcel-setting-zoom-value")[0];
         if (zoomSlider && zoomValue) {
             zoomSlider.addEventListener("input", (event) => {
                 const zoomLevel = parseInt(event.target.value);
@@ -329,7 +359,7 @@ class SheexcelActorSheet extends ActorSheet {
     }
 
     _setupHideMenu(html, iframe) {
-        const hideMenuCheckbox = html.find("#sheexcel-hide-menu")[0];
+        const hideMenuCheckbox = html.find("#sheexcel-setting-hide-menu")[0];
         if (hideMenuCheckbox) {
             hideMenuCheckbox.addEventListener("change", async (event) => {
                 const hideMenu = event.target.checked;
@@ -350,7 +380,12 @@ class SheexcelActorSheet extends ActorSheet {
     }
 
     async close(...args) {
-        await this._saveZoomLevel();
+        await this._saveFlags();
+        
+        return super.close(...args);
+    }
+
+    async _saveFlags() {
         const flags = {
             zoomLevel: this._currentZoomLevel,
             sidebarCollapsed: this._sidebarCollapsed,
@@ -362,11 +397,6 @@ class SheexcelActorSheet extends ActorSheet {
         await this.actor.update({
             'flags.sheexcel': flags
         });
-        return super.close(...args);
-    }
-
-    async _saveZoomLevel() {
-        await this.actor.setFlag("sheexcel", "zoomLevel", this._currentZoomLevel);
     }
 
     getSheexcelValue(keyword) {
