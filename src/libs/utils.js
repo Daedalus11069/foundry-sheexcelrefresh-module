b25a1ed8ac36521f8767001aa5c2bd6015d19289
daedalus11069 <daedalus969@gmail.com>
2025-02-06 15:51:32 -0800

    Improve identifier value generation

--- src/libs/utils.js
+++ src/libs/utils.js
@@ -1,6 +1,6 @@
 const stringToIdentifier = str => {
   // Replace invalid characters with underscores and remove leading/trailing underscores
-  let identifier = str.replace(/[^a-zA-Z0-9_$]/g, "_").replace(/^_+|_+$/g, "");
+  let identifier = str.replace(/[^a-zA-Z0-9_$]/g, "").replace(/^_+|_+$/g, "");
 
   // Ensure the identifier doesn't start with a number
   if (/^[0-9]/.test(identifier)) {
